d76c8524629145443b6d2133da2430b04b7c440f
Skywolf <glushnev81@gmail.com>
2022-07-12 20:56:14 +0300

    Урок 2

---
+++ src/lesson2/Lesson2.java
@@ -0,0 +1,12 @@
+package lesson2;
+
+public class Lesson2 {
+    public static void main(String[] args) {
+        String text = "Это текст";
+        int number = 10;
+        String newText = text.toLowerCase();
+
+        System.out.println(text);
+        System.out.println(newText);
+    }
+}
