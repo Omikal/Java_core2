cff91709d6f7918cf3e2564e000c98b3994be160
Skywolf <glushnev81@gmail.com>
2022-07-12 22:07:37 +0300

    Урок 2 конец

--- src/lesson2/Lesson2.java
+++ src/lesson2/Lesson2.java
@@ -2,14 +2,23 @@ package lesson2;
 
 public class Lesson2 {
     public static void main(String[] args) {
-        String text = "Это текст";
+        String text = "Это текст\n";
         int number = 10;
+//        Нижний регистр
         String newText = text.toLowerCase();
+//        Верхний регистр
+        String newText2 = text.toUpperCase();
+//        замена строки
+        String txt = text.replace("текст","нет");
+//         Повторить строку n раз
+        String repeatedText = text.repeat(10);
 
         int clet = text.length();
 
         System.out.println(text);
         System.out.println(newText);
+        System.out.println(newText2);
         System.out.println(clet);
+        System.out.println(repeatedText);
     }
 }
