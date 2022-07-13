d4457c544743916ceda694da4007f8dd4367f81d
Skywolf <glushnev81@gmail.com>
2022-07-13 20:56:46 +0300

    HW1 добавил задание 3

--- src/HW1_Basic/HW1.java
+++ src/HW1_Basic/HW1.java
@@ -1,5 +1,6 @@
 package HW1_Basic;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class HW1 {
@@ -13,13 +14,20 @@ public class HW1 {
         System.out.println(text);
 
 //          Задание 2
-        Scanner s = new Scanner(System.in);
-        System.out.println("Введите рост в метрах (прим. 1,78) и нажмите enter\n");
-        float height = s.nextFloat();
-        System.out.println("Введите вес в кг (прим. 75,96) и нажмите enter\n");
-        float weight = s.nextFloat();
-        float index = weight/(height*height);
-        System.out.println("Ваш индекс массы тела =" + ' ' + index);
+//        Scanner s = new Scanner(System.in);
+//        System.out.println("Введите рост в метрах (прим. 1,78) и нажмите enter\n");
+//        float height = s.nextFloat();
+//        System.out.println("Введите вес в кг (прим. 75,96) и нажмите enter\n");
+//        float weight = s.nextFloat();
+//        float index = weight/(height*height);
+//        System.out.println("Ваш индекс массы тела =" + ' ' + index);
 
+//          Задание 3
+        char[] letters = new char[] {'a', 'b', 'c', 'd', 'e'};
+        String charText = String.valueOf(letters);
+        System.out.println(charText);
+        letters[3] = 'h';
+        String charText2 = String.valueOf(letters);
+        System.out.println(charText2);
     }
 }
