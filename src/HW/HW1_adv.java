5d8e3488ee6c8e17a3f711539fe66d43806602bb
Skywolf <glushnev81@gmail.com>
2022-07-13 21:13:58 +0300

    HW1_adv задание 1

---
+++ src/HW/HW1_adv.java
@@ -0,0 +1,12 @@
+package HW;
+
+public class HW1_adv {
+    public static void main(String[] args) {
+//    Задание 1
+        String num = "234";
+        String txt = "some_text";
+        int num2 = Integer.parseInt(num);
+        int lng = txt.length();
+        System.out.println(num2+lng);
+    }
+}
