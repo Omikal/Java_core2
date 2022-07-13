d93dfac40c3dfaea561d483605945e050cf9e254
Skywolf <glushnev81@gmail.com>
2022-07-13 21:25:56 +0300

    HW1_adv задание 2

--- src/HW/HW1_adv.java
+++ src/HW/HW1_adv.java
@@ -2,11 +2,18 @@ package HW;
 
 public class HW1_adv {
     public static void main(String[] args) {
-//    Задание 1
+//      Задание 1
         String num = "234";
         String txt = "some_text";
         int num2 = Integer.parseInt(num);
         int lng = txt.length();
         System.out.println(num2+lng);
+
+//      Задание 2
+//      Посчитать (a+b)^2 = ?, при a=3, b=5
+        double number1 = 3;
+        double number2 = 5;
+        double number3 = Math.pow(number1 + number2 , 2);
+        System.out.println(number3);
     }
 }
