20d43fd9f925eaa3bd73fa2fbabc9b04fea63506
Skywolf <glushnev81@gmail.com>
2022-07-22 21:40:15 +0300

    Собака

--- src/HW/dog.java
+++ src/HW/dog.java
@@ -5,12 +5,23 @@ public class Dog {
     private String name;
     private int age;
     private String size;
-    public Dog (String nameDog, int ageDog, String sizeDog) {
+    private String color;
+    public Dog (String nameDog, int ageDog, String sizeDog, String colorDog) {
         this.name = nameDog;
         this.age = ageDog;
         this.size = sizeDog;
+        this.color = colorDog;
     }
-    public String getDogName() {
+    public String getDogName () {
         return prefixDog + this.name;
+    }
+    public int getDogAge () {
+        return this.age;
+    }
+    public String getDogSize () {
+        return this.size;
+    }
+    public String getDogColor () {
+        return this.color;
     }
 }
