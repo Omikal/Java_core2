b85faf08ec60ca259f53b2297f99e1c1eaa239e4
Omikal <glushnev81@gmail.com>
2022-07-25 01:16:58 +0300

    склад

--- src/HW/Worker.java
+++ src/HW/Worker.java
@@ -1,7 +1,33 @@
 package HW;
 
 public class Worker {
-    private String nameWorker;
-    private int ageWorker;
-    private int productDestroyedCountWorker;
+    private static String pref = "Рабочий";
+    private String name;
+    private int productDestroyed = 0;
+    private Storage storage;
+    public Worker(Storage storage) {
+        this.storage = storage;
+    }
+    public Worker (String nameWorker, int productDestroyedByWorker) {
+        this.name = nameWorker;
+        this.productDestroyed = productDestroyedByWorker;
+    }
+    public String getNameWorker () {
+        return this.name;
+    }
+    public int getProductDestroyed () {
+        return productDestroyed;
+    }
+    public void productDestroyed (int productDestroyed) {
+        this.productDestroyed = productDestroyed;
+    }
+    public void poteryaPoter () {
+        if (storage.getProductCountStorage() > 0) {
+            storage.setProductCountStorage(storage.getProductCountStorage() - 1);
+            productDestroyed = productDestroyed + 1;
+            System.out.println("Ура, я испортил водку!");
+        } else {
+            System.out.println("Больше нет водки, чтобы испортить!");
+        }
+    }
 }
