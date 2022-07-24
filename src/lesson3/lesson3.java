67039bdfc9ea3ec3fa86e6eaa51be3b707c21fdf
Omikal <glushnev81@gmail.com>
2022-07-25 01:47:48 +0300

    les3

---
+++ src/lesson3/lesson3.java
@@ -0,0 +1,14 @@
+package lesson3;
+
+public class lesson3 {
+    public static void main(String[] args) {
+        int num1 = 11;
+        int num2 = 11;
+        boolean eqv = num1 == num2;
+        if (eqv) {
+            System.out.println("числа равны");
+        } else {
+            System.out.println("числа не равны");
+        }
+    }
+}
