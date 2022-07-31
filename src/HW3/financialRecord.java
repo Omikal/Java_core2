132a48dbe0bab5230bbcf16f6dc4fae514d80941
Omikal <glushnev81@gmail.com>
2022-07-31 15:22:56 +0300

    finRec

---
+++ src/HW3/financialRecord.java
@@ -0,0 +1,27 @@
+package HW3;
+
+public class financialRecord {
+    private int incomes;
+    private int outcomes;
+    public financialRecord(int finIn, int finOut) {
+        this.incomes = finIn;
+        this.outcomes = finOut;
+    }
+
+
+    public int getOutcomes() {
+        return outcomes;
+    }
+
+    public void setOutcomes(int outcomes) {
+        this.outcomes = outcomes;
+    }
+
+    public int getIncomes() {
+        return incomes;
+    }
+
+    public void setIncomes(int incomes) {
+        this.incomes = incomes;
+    }
+}
