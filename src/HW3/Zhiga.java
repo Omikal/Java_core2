3175b0135c5aacf1f25a73a24e7fed6ac73611b4
Omikal <glushnev81@gmail.com>
2022-07-31 11:58:13 +0300

    zhiga

--- src/HW3/Zhiga.java
+++ src/HW3/Zhiga.java
@@ -1,6 +1,18 @@
 package HW3;
 
 public class Zhiga extends Car {
+    private String name;
+    private String color;
+    public Zhiga(String zhigaName, String zhigaColor) {
+        this.name = zhigaName;
+        this.color = zhigaColor;
+    }
+    public String getZhigaColor() {
+        return this.color;
+    }
+    public String getZhigaName() {
+        return this.name;
+    }
     //ломается
     public void breakOut() {
         System.out.println("Жигули сломалась");
