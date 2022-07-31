0dca0d7a16ada1ef6ab80e8a5e6709222769bc66
Omikal <glushnev81@gmail.com>
2022-07-31 12:18:59 +0300

    toyota

--- src/HW3/Toyota.java
+++ src/HW3/Toyota.java
@@ -1,20 +1,11 @@
 package HW3;
 
 public class Toyota extends Car {
-    private String name;
-    private String color;
-    public Toyota(String toyotaName, String toyotaColor) {
-        this.name = toyotaName;
-        this.color = toyotaColor;
-    }
-    public String getToyotaColor() {
-        return this.color;
-    }
-    public String getToyotaName() {
-        return this.name;
+    public Toyota(String name) {
+        super(name);
     }
     //включает музыку
     public void music() {
-        System.out.println("Тойота включает музыку");
+        System.out.println(this.name + " включает музыку");
     }
 }
